2c7037c3b0972799b94acc56b50b129daf28d7db
Pedro Sanchez Petul <pedro.petul@ciencias.unam.mx>
2021-10-20 01:14:52 -0700

    test

---
+++ Ponycode/C++/hola.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+ using namespace std;
+
+ int main(){
+     cout<<"hola pony"<<endl;
+     return 0;    
+ }
---
+++ Ponycode/C/hola.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+
+int main(){
+    printf("Hola pony");
+    return 0;
+}
